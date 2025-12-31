d8da96c2480c2cb39f6678fa9a785926c5aa4e7b
abhra <patra.abhra97@gmail.com>
2025-12-31 18:19:00 +0530

    updated schema to store ram, storage, color variant

--- models/wishlist.models.js
+++ models/wishlist.models.js
@@ -2,6 +2,20 @@ const mongoose = require("mongoose");
 
 const wishlistSchema = new mongoose.Schema({
     productId : String ,
+    variant: {
+      color: {
+        type: String,
+        required: true,
+      },
+      ram: {
+        type: String,
+        required: true,
+      },
+      storage: {
+        type: String,
+        required: true,
+      },
+    },
 },
 {
     timestamps: true,
